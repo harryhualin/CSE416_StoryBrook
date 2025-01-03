b0f7e26c1d0648579c53bfebbe9da71911abac7a
hua lin <harryhualin@gmail.com>
2025-01-03 15:12:28 -0500

    typo

--- server/index.js
+++ server/index.js
@@ -8,16 +8,13 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 const app = express()
 
+/* Access-Control-Allow-Origin: * */
+app.use(cors());
+
+
 // SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ limit: '200mb',extended: true }))
 
-app.use(cors({
-
-    origin: ["http://localhost:3000","https://sbrook.herokuapp.com","*"],        //http://localhost:3000
-
-    credentials: true
-}))
-//
 app.use(express.json({limit: '200mb'}))
 app.use(cookieParser())
 
@@ -42,4 +39,14 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 //         res.sendFile('/app/client/build/index.html');
 //         })
 //     }
+
+
+app.get('/', function getHello(req, res, next) {
+    res.send('Hello World!');
+});
+
+
+
+// open port to allow connection
+app.listen(8080);
 
